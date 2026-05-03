12985b3814295c404f90c35e034e5c7fcfcdc11d
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2026-05-03 21:05:22 +1000

    Did a little debugging on the inventory
    It doesn't change when you press the side to side anymore but will still play one animation before switching tools

--- 10ct-harrow's-garden/scenes/ui/tools_panel.gd
+++ 10ct-harrow's-garden/scenes/ui/tools_panel.gd
@@ -13,3 +13,26 @@ func _on_tool_axe_pressed() -> void:
 
 func _on_tool_tilling_pressed() -> void:
 	ToolManager.select_tool(DataTypes.Tools.TillGround)
+
+
+func _on_tool_watering_can_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.WaterCrops)
+
+
+func _on_tool_carrot_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.PlantCarrot)
+
+
+func _on_tool_tomato_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.PlantTomato)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			ToolManager.select_tool(DataTypes.Tools.None)
+			tool_axe.release_focus()
+			tool_tilling.release_focus()
+			tool_watering_can.release_focus()
+			tool_carrot.release_focus()
+			tool_tomato.release_focus()
